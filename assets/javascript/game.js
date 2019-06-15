295f56d5067d16055864aef4e03ad7e768ddbdf1
Matt Jennings <matt@mattjennings.net>
2019-06-15 16:46:32 -0700

    fixed click event for $yourCharacterLinks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,35 @@ $(document).ready(function() {
     // Start game set to false
     var startGame = false;
 
+    var $yourCharacterLinks = $('#your-character a');
+    var $attackerLinks = $('#attacker a');
+
+    var $messages = $('#messages');
+
     $('#game-state-btn > button').on('click', function() {
         startGame = true;
+        $messages.removeClass('d-none');
+        $(this).text('Restart Game');
     });
 
-    $('').on('click', function(e) {
+    $yourCharacterLinks.on('click', function(e) {
         e.preventDefault();
+
+        if(startGame) {
+            $yourCharacterLinks.addClass('d-none');
+            $(this).removeClass('d-none');
+            
+            console.log($(this).attr('data-char'));
+
+            for(var i = 0; i < $attackerLinks.length; i++) {
+                if($($attackerLinks[i]).attr('data-char') === $(this).attr('data-char')) {
+                    $($attackerLinks[i]).addClass('d-none');
+                }
+                else {
+                    $($attackerLinks[i]).removeClass('d-none');
+                }
+            }
+        }
     });
 
 });
