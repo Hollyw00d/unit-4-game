76914b68d475b47c7f7d6a78f0ec5092703e341e
Matt Jennings <matt@mattjennings.net>
2019-06-15 20:05:03 -0700

    successfully selected attacker name

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,14 @@ $(document).ready(function() {
     // Start game set to false
     var startGame = false;
 
-    var healthPointsObiWanKenobi;
-    var healthPointsLukeSkywalker;
-    var healthPointsDarthSidious;
-    var healthPointsDarthMaul;
+    var attackBegun = false;
+    var selectedAttacker;
+    var selectedDefender;
+
+    var healthPointsObiWanKenobi = 120;
+    var healthPointsLukeSkywalker = 100;
+    var healthPointsDarthSidious = 150;
+    var healthPointsDarthMaul = 180;
 
     var attackPowerObiWanKenobi;
     var attackPowerLukeSkywalker;
@@ -22,23 +26,34 @@ $(document).ready(function() {
     var $defenderLinks = $('#defender a');
     var $gameStartBtn = $('#game-state-btn > button');
     var $messages = $('#messages');
+    var $healthPoints = $('.health-points');
 
     $gameStartBtn.on('click', function() {
         startGame = true;
-        $messages.removeClass('d-none');
-        $(this).text('Restart Game');
+        if(startGame) {
+            $messages.text('Click on a Character');
+            $messages.removeClass('d-none');
+            $(this).text('Restart Game');
+        }
     });
 
     $yourCharacterLinks.on('click', function(e) {
         e.preventDefault();
 
-        if(startGame) {
+        if(startGame && !attackBegun) {
+
+            attackBegun = true;
+
+            $messages.text('Click on an Enemy Available to Attack');
+
             $yourCharacterLinks.addClass('d-none');
             $(this).removeClass('d-none').addClass('active');
 
             for(var i = 0; i < $attackerLinks.length; i++) {
                 if($($attackerLinks[i]).attr('data-char') === $(this).attr('data-char')) {
                     $($attackerLinks[i]).addClass('d-none');
+                    var $attackerSelectDataChar = $($attackerLinks[i]).attr('data-char');
+                    selectedAttacker = $("#attacker a[data-char='" + $attackerSelectDataChar + "']").attr('aria-label');
                 }
                 else {
                     $($attackerLinks[i]).removeClass('d-none').addClass('active');
@@ -49,17 +64,19 @@ $(document).ready(function() {
 
     $attackerLinks.on('click', function(e) {
         e.preventDefault();
-        if(startGame) {
+        if(startGame && !$defenderLinks.hasClass('active')) {
             $(this).addClass('d-none');
 
             for(var i = 0; i < $defenderLinks.length; i++) {
                 if($($defenderLinks[i]).attr('data-char') === $(this).attr('data-char')) {
                     $($defenderLinks[i]).removeClass('d-none').addClass('active');
+                    
                 }
                 else {
                     $($defenderLinks[i]).addClass('d-none');
                 }
-            }            
+            }  
+            
         }
     });
 
